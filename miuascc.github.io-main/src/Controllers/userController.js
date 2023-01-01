7b2ebfe920fb49335d8f0bb130ac00e6cde138e5
adham347 <adham347@gmail.com>
2023-01-01 16:53:49 +0200

    file structure and addPost updates

--- miuascc.github.io-main/src/Controllers/userController.js
+++ miuascc.github.io-main/src/Controllers/userController.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-    import {
-        getAuth,
-        createUserWithEmailAndPassword,
-        signInWithEmailAndPassword,
-        signOut
-    } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
-    import { getDatabase, set, ref, update, onValue } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getDatabase, set, ref, update, onValue } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
 
 
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        // Your configs
-        apiKey: "AIzaSyBRC3Z3hwO2K5GL4GuYT6lj2uHF81HhHIE",
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    // Your configs
+    apiKey: "AIzaSyBRC3Z3hwO2K5GL4GuYT6lj2uHF81HhHIE",
     authDomain: "trialsocialmedia-24fd5.firebaseapp.com",
     databaseURL: "https://trialsocialmedia-24fd5-default-rtdb.firebaseio.com",
     projectId: "trialsocialmedia-24fd5",
@@ -22,19 +22,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebas
     messagingSenderId: "489989876046",
     appId: "1:489989876046:web:a6ee092801e1da5b45961f",
     measurementId: "G-NK7X1BMFS1"
-    };
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
-    const database = getDatabase(app);
-    var btnRegister = document.getElementById('submit');
-    var login = document.getElementById('loginBtn');
-    var logout =  document.getElementById('logoutShow');
-    
-if(login)
-{
-    login.addEventListener('click', function(e) {
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase(app);
+var btnRegister = document.getElementById('submit');
+var login = document.getElementById('loginBtn');
+var logout = document.getElementById('logoutShow');
+
+if (login) {
+    login.addEventListener('click', function (e) {
 
         e.preventDefault();
         var email = document.getElementById('email').value;
@@ -82,26 +81,24 @@ if(login)
     });
 
 }
-    
 
-    
-  
 
-if(btnRegister)
-{
-    btnRegister.addEventListener('click', function(e)
-    {
+
+
+
+if (btnRegister) {
+    btnRegister.addEventListener('click', function (e) {
         e.preventDefault();
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
-      var confirmPassword = document.getElementById('cPassword').value;
-      var teamPos = document.getElementById('teams').value;
-      var username = document.getElementById('uname').value;
-      var fname = document.getElementById('fullname').value;
-    
-      if(password == confirmPassword){
-    
-        createUserWithEmailAndPassword(auth, email, password)
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        var confirmPassword = document.getElementById('cPassword').value;
+        var teamPos = document.getElementById('teams').value;
+        var username = document.getElementById('uname').value;
+        var fname = document.getElementById('fullname').value;
+
+        if (password == confirmPassword) {
+
+            createUserWithEmailAndPassword(auth, email, password)
                 .then((userCredential) => {
                     // Signed in
                     const user = userCredential.user;
@@ -111,14 +108,14 @@ if(btnRegister)
                         email: email,
                         password: password,
                         clubPosition: teamPos,
-                        isAdmin:0,
+                        isAdmin: 0,
                         fullname: fname
                     })
                         .then(() => {
                             // Data saved successfully!
                             alert('User created successfully');
                             location.replace('login.html');
-            
+
                         })
                         .catch((error) => {
                             // The write failed...
@@ -131,56 +128,55 @@ if(btnRegister)
                     // ..
                     alert(errorMessage);
                 });
-    
-            }
-    
-            else{
-                alert('Password not matching');
-            }
-    
+
+        }
+
+        else {
+            alert('Password not matching');
+        }
+
     });
 
 }
 
 var announ = document.getElementById('ann');
-      
-      //var firebaseRef = database().ref();
-auth.onAuthStateChanged(function(user) {
-    
+
+//var firebaseRef = database().ref();
+auth.onAuthStateChanged(function (user) {
+
     if (user) {
         // User is signed in.
-    
+
         var user = auth.currentUser;
-    
-        if(user != null){
+
+        if (user != null) {
             var isAdminRef = ref(database, 'users/' + user.uid + '/isAdmin');
             onValue(isAdminRef, (snapshot) => {
                 var data = snapshot.val();
                 console.log(data);
-                if(data)
-                { 
+                if (data) {
                     document.getElementById('adminShow').style.display = "block";
                     document.getElementById('addPostShow').style.display = "block";
 
                 }
-              });
-              document.getElementById('announShow').style.display = "block";
-              document.getElementById('logoutShow').style.display = "block";
-              document.getElementById('loginShow').style.display = "none";
-              document.getElementById('registerShow').style.display = "none";
+            });
+            document.getElementById('announShow').style.display = "block";
+            document.getElementById('logoutShow').style.display = "block";
+            document.getElementById('loginShow').style.display = "none";
+            document.getElementById('registerShow').style.display = "none";
 
         }
-    
-      } else {
+
+    } else {
         document.getElementById('loginShow').style.display = "block";
         document.getElementById('registerShow').style.display = "block";
-        
-        
-      }
+
+
+    }
 });
 
-if(logout){
-    logout.addEventListener('click',function(e){
+if (logout) {
+    logout.addEventListener('click', function (e) {
         e.preventDefault();
         auth.signOut();
         alert('Logged out successfully');
@@ -189,7 +185,7 @@ if(logout){
 
     });
 }
-    
 
-   
-    
+
+
+
